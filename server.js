e5c1d1692cc5aa1401f0cf4292cd932c58878014
Alex Zai <azai91@gmail.com>
2015-01-07 12:26:10 -0800

    (refactor) remove client and bower folder

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname));
+app.use('*', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 3000);
