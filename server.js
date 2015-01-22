b06b5d54369932717fa6a4f18127a07e4ba2785e
Alex Zai <azai91@gmail.com>
2015-01-22 15:31:09 -0800

    (feat) add style with dynamic routing

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
 
+
+
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/skype-bg.png', function(req, res) {
+  res.sendFile(__dirname + '/skype-bg.png');
+});
+
 app.use('*', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+
+// app.use(express.static(__dirname));
 
 app.listen(process.env.PORT || 3000);
