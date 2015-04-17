26bdf1982eb6644247217681c24721924862e402
Alex Zai <azai91@gmail.com>
2015-04-16 18:25:10 -0700

    (feat) remove extra assets

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var app = express();
-var config = require('config');
 var morgan = require('morgan');
 
 // app.use(morgan('combined'));
 
-app.get('/style.css', function(req, res) {
-  res.sendFile(__dirname + '/style.css');
-});
+// app.get('/style.css', function(req, res) {
+//   res.sendFile(__dirname + '/style.css');
+// });
 
-app.get('/skype-bg.png', function(req, res) {
-  res.sendFile(__dirname + '/skype-bg.png');
-});
+// app.get('/skype-bg.png', function(req, res) {
+//   res.sendFile(__dirname + '/skype-bg.png');
+// });
 
-app.use('*', function(req, res) {
-  res.sendFile(__dirname + '/' + config.get('file'));
-});
+app.use('*', express.static(__dirname));
 
 app.listen(process.env.PORT || 3000);
